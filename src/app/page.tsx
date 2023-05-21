f9da6372126c9cb7d12f2fe9e449f00b931c3be9
klim <klimentykk@gmail.com>
2023-05-21 14:23:07 +0400

    feat: fully adaptive about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Introduction } from '../components/sections/introduction/Introduction'
 import { Reputation } from '@/components/sections/reputation/Reputation'
+import { About } from '@/components/sections/about/About'
 
 export default function Home() {
 	return (
 		<main>
 			<Introduction />
 			<Reputation />
+			<About />
 		</main>
 	)
 }
---
+++ src/components/sections/about/About.tsx
@@ -0,0 +1,50 @@
+'use client'
+import React from 'react'
+import useMediaQuery from '@/hooks/useMediaQuery'
+import Image from 'next/image'
+
+import ImageData from '../../../assets/other/OldMan.jpg'
+
+export const About: React.FC = () => {
+	const isNotMobile = useMediaQuery('(min-width: 770px)')
+	return (
+		<section className='about'>
+			{isNotMobile && (
+				<div className='about__img'>
+					<Image
+						src={ImageData.src}
+						blurDataURL={ImageData.blurDataURL}
+						alt='About image'
+						fill
+					/>
+				</div>
+			)}
+
+			<div className='about__info'>
+				<h3 className='about__heading'>About us</h3>
+
+				{!isNotMobile && (
+					<div className='about__img'>
+						<Image
+							src={ImageData.src}
+							blurDataURL={ImageData.blurDataURL}
+							alt='About image'
+							fill
+						/>
+					</div>
+				)}
+
+				<p className='about__text'>
+					For more than 30 years we have been delivering world-class
+					construction and we’ve built many lasting relationships along the way.
+				</p>
+				<p className='about__text'>
+					We’ve matured into an industry leader and trusted resource for those
+					seeking quality, innovation and reliability when building in the U.S.
+				</p>
+
+				<button className='about__btn'>More on our history</button>
+			</div>
+		</section>
+	)
+}
